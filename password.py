ccf4f68614ce2a6e6cd9fd78add4b8e9e95051bd
Debmalya Biswas <68019329+AnTIdoTe003@users.noreply.github.com>
2021-12-11 20:55:56 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,27 @@
+import random
+
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+					'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+					'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+					'z']
+
+UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+					'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
+					'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+					'Z']
+
+SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+		'*', '(', ')', '<']
+
+
+COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+password = ""
+for i in range (12):
+    password=password+random.choice(COMBINED_LIST)
+s=input("Enter the name of the file ")
+fh=open('mypass'+str(s)+'.txt', 'w')
+fh.write("This is my new password.\n")
+fh.write(password)
+fh.close()
+
